9bfdf821a9e36a455b1f16160ee11d7fde7a622a
Flaner6 <christosmpalotis@gmail.com>
2021-07-25 19:10:53 +0300

    github deployment

---
+++ README.md
@@ -0,0 +1,2 @@
+Deployed on Heroku
+https://nameless-sierra-32335.herokuapp.com
---
+++ src/resources/views/products/index.blade.php
@@ -0,0 +1,53 @@
+@extends('products.layout')
+ 
+@section('content')
+
+    <div class ="jumbotron">
+        <div class="col-lg-12 margin-tb">
+            <div class="col-lg-8 col-md-6 col-sm-6 col-xs-6 offset-3 float-md-center">
+                <h2 style="text-align:center;">This is the list of products</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-success" href="{{ route('products.create') }}"> Create New Product</a>
+            </div>
+        </div>
+    </div>
+   
+    @if ($message = Session::get('success'))
+        <div class="alert alert-success">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
+   
+    <table class="table table-bordered">
+        <tr>
+            <th>No</th>
+            <th>Name</th>
+            <th>Details</th>
+            <th width="280px">Action</th>
+        </tr>
+        @foreach ($products as $product)
+        <tr>
+            <td>{{ ++$i }}</td>
+            <td>{{ $product->name }}</td>
+            <td>{{ $product->detail }}</td>
+            <td>
+                <form action="{{ route('products.destroy',$product->id) }}" method="POST">
+   
+                    <a class="btn btn-info" href="{{ route('products.show',$product->id) }}">Show</a>
+    
+                    <a class="btn btn-primary" href="{{ route('products.edit',$product->id) }}">Edit</a>
+   
+                    @csrf
+                    @method('DELETE')
+      
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </table>
+  
+    {!! $products->links() !!}
+      
+@endsection
